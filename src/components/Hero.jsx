f8f6ef34d55d40c7d8257ab06406bdd833db90d4
Anto037 <antonioflorea39@gmail.com>
2026-06-08 18:27:14 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import FadeSection from "./FadeSection";
-import Me from "../assets/me.png";
+import Me from "../assets/Me.png";
 import LanguageToggle from "./LanguageToggle";
 import { t } from "../i18n";
 
